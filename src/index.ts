0eb095801c3bc6ac7f4a121bf1cb64bd90b168a9
medyolangs <medyochain@gmail.com>
2021-10-07 17:09:18 +0800

    deploying to heroku

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ import { SetCookie } from "./Resolvers/SetCookie";
     app.use(express.json());
     app.use(cookieParser());
     //app.use(cors());
-    app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+    app.use(cors({ origin: "https://dogentures.netlify.app/", credentials: true }));
 
     app.use("/", router);
     app.use("/images", express.static(join(__dirname, "images")));
